37954353010555635edc168e0d52aed7b4a7e385
Med Elmostapha <dedijekani@gmail.com>
2026-04-07 22:20:15 +0000

    create add note screen

---
+++ lib/features/notes/screens/add_note_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AddNoteScreen extends StatelessWidget {
+  const AddNoteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleController = TextEditingController();
+    final contentController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Add Note")),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: const InputDecoration(labelText: 'Title'),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: contentController,
+              decoration: const InputDecoration(labelText: 'Content'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
